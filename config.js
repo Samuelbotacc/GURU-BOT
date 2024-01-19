2b37e6a8c32e494e0e8beb174e080966ded3e881
Samuelbotacc <153863055+Samuelbotacc@users.noreply.github.com>
2024-01-19 12:24:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['254703324883', 'SAVAGE', true],
+  ['254703324883', 'RATTY', true], 
   [''] 
 ] //Number of owners
 
